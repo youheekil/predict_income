ba0eb88bf250afeea7a9998cc46b1b5544743f0e
Youhee Kil <youhee.kil@gmail.com>
2022-02-17 14:33:08 +0100

    procfile edited

--- api_test.py
+++ api_test.py
@@ -5,7 +5,7 @@ Tesing Heroku App test
 import requests
 
 
-url = "http://0.0.0.0:8000/prediction?age=45&workclass=Self-emp-inc&fnlgt=40&education=Prof-school&education_num=14&marital_status=Separated&occupation=Sales&relationship=Not-in-family&race=White&sex=Male&capital_gain=10000&capital_loss=1000&hours_per_week=45&native_country=United-States"
+url = "https://income-prediction-ml-yh.herokuapp.com/prediction?age=45&workclass=Self-emp-inc&fnlgt=40&education=Prof-school&education_num=14&marital_status=Separated&occupation=Sales&relationship=Not-in-family&race=White&sex=Male&capital_gain=10000&capital_loss=1000&hours_per_week=45&native_country=United-States"
 
 
 def try_api():
